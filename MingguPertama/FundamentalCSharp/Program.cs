ce239221bc1727ddcd2a147f531aa9ce626532a1
Iskandar-dev <putra.iskandar@outlook.com>
2022-01-04 21:35:17 +0700

    initial commit;

---
+++ MingguPertama/FundamentalCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Datatype di C#
+        int x = 6; // Bilangan Bulat
+        double pi = 3.14; // Bilangan decimal
+        char f = 'H'; // Bilangan Bulat
+        string str = "Hello World!"; // String menyimpan Karakter dan Number/Alphanumeric
+        bool kondisi = false; // True Or False
+
+
+        Console.WriteLine(x);
+        Console.WriteLine(pi);
+        Console.WriteLine(f);
+        Console.WriteLine(str);
+        Console.WriteLine(kondisi);
+
+        Console.ReadLine();
+    }
+}
